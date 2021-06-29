920a649b8c0af8934fcba21b03258a10c918f661
Correa <darlancont@gmail.com>
2021-06-29 11:50:54 -0300

    Commit catalogo jogos

---
+++ ExemploApiCatalogoJogos/Exceptions/JogoJaCadastradoException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ExemploApiCatalogoJogos.Exceptions
+{
+    public class JogoJaCadastradoException : Exception
+    {
+        public JogoJaCadastradoException()
+            : base(" O Jogo ja está cadastrado")
+        { }
+    }
+}
